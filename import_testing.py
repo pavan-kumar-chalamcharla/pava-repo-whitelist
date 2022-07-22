813c43942384bb1f82f54e9f6f806a1dd17d693d
pavan.kumarchalamcharla@databricks.com <pavan.kumarchalamcharla@databricks.com>
2022-07-22 09:21:11 +0000

    push_testing

---
+++ import_testing.py
@@ -0,0 +1,34 @@
+# Databricks notebook source
+from testing_folder.sample import n_to_mth
+n_to_mth(3, 4)
+
+# COMMAND ----------
+
+testing_folder/sample.py
+
+# COMMAND ----------
+
+from testing_folder.mysql_class import MYSQL
+print(MYSQL.PORT)
+
+# COMMAND ----------
+
+from importing_corss_repos.mysql_class import MYSQL
+print(MYSQL.PORT)
+
+# COMMAND ----------
+
+import sys
+import os
+ 
+# In the command below, replace <username> with your Databricks user name.
+sys.path.append(os.path.abspath('/Workspace/Repos/pavan.kumarchalamcharla@databricks.com/kumar'))
+
+# COMMAND ----------
+
+from importing_corss_repos.mysql_class import MYSQL
+print(MYSQL.PORT)
+
+# COMMAND ----------
+
+# MAGIC %sh ls /Workspace/Repos/pavan.kumarchalamcharla@databricks.com
---
+++ testing_folder/mysql_class.py
@@ -0,0 +1,4 @@
+class MYSQL:
+    PORT = 3306
+    DATABASE = 'mydb'
+    DATABASE_TABLES = ['tb_users', 'tb_groups']
